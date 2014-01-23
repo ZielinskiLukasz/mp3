df4fe799883e22a0fd7dabc058f0b00d9ca800dc
Sammie S. Taunton <diemuzi@gmail.com>
2014-01-22 19:18:39 -0600

    Added Missing Translation Configuration

--- config/module.config.php
+++ config/module.config.php
@@ -54,6 +54,16 @@ return array(
             )
         )
     ),
+    'translator'      => array(
+        'translation_file_patterns' => array(
+            array(
+                'type'        => 'gettext',
+                'base_dir'    => __DIR__ . '/../language',
+                'pattern'     => '%s.mo',
+                'text_domain' => 'mp3'
+            )
+        )
+    ),
     'controllers'     => array(
         'invokables' => array(
             'Mp3\Controller\Mp3' => 'Mp3\Controller\Mp3Controller'
